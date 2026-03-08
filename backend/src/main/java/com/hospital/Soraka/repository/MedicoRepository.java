64655c984173a9a9f7993b14c4b7618efc5d0f1a
lun-code <lun.code01@gmail.com>
2026-03-08 23:07:44 +0100

    fix(repository): añadir import de Optional en MedicoRepository

--- backend/src/main/java/com/hospital/Soraka/repository/MedicoRepository.java
+++ backend/src/main/java/com/hospital/Soraka/repository/MedicoRepository.java
@@ -4,6 +4,8 @@ import com.hospital.Soraka.entity.Medico;
 import com.hospital.Soraka.entity.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.util.Optional;
+
 
 @Repository
 public interface MedicoRepository extends JpaRepository<Medico,Long> {
