0c42bca881982bb5a2a34ae4c3ae648a1ef9f5bb
lun-code <lun.code01@gmail.com>
2026-03-12 13:29:01 +0100

    Fix: Agregar import

--- backend/src/main/java/com/hospital/Soraka/security/SecurityConfig.java
+++ backend/src/main/java/com/hospital/Soraka/security/SecurityConfig.java
@@ -19,7 +19,7 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-
+import org.springframework.beans.factory.annotation.Value;
 import java.util.List;
 
 /**
