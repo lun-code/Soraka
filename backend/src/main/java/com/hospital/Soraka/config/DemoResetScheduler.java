c895311782966bc974386ac66e07f3216259de1c
lun-code <lun.code01@gmail.com>
2026-07-09 02:33:04 +0200

    Migración a render

--- backend/src/main/java/com/hospital/Soraka/config/DemoResetScheduler.java
+++ backend/src/main/java/com/hospital/Soraka/config/DemoResetScheduler.java
@@ -7,7 +7,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.transaction.support.TransactionTemplate;
 
 @Component
